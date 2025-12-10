2ef22b6450c1ed10084cff680089e84d91bad363
turbo-nut <B00152224@mytudublin.ie>
2025-12-10 20:46:37 +0000

    demonstration

--- src/app.rs
+++ src/app.rs
@@ -1,28 +1,29 @@
+use egui::Align2;
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
+use egui_toast;
+use egui_toast::{Toast, ToastKind, ToastOptions, Toasts};
+use std::default::Default;
+use std::process::Command;
+
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    #[serde(skip)] // This how you opt-out of serialization of a field
+#[derive(Default)]
+pub struct PrototypeUI {
+    name: String,
+    desc: String,
+    #[serde(skip)] // disable auto-serialization
     value: f32,
+    show_nextcloud_viewport: bool,
+    _temp_value: usize,
+    _title: String,
+    _text: String,
 }
 
-impl Default for TemplateApp {
-    fn default() -> Self {
-        Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
-        }
-    }
-}
 
-impl TemplateApp {
+impl PrototypeUI {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
+
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
 
         // Load previous app state (if any).
@@ -32,23 +33,211 @@ impl TemplateApp {
         } else {
             Default::default()
         }
-    }
-}
 
-impl eframe::App for TemplateApp {
-    /// Called by the framework to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+    //show the nextcloud installation guide
+    pub fn show_guide(&mut self, ctx: &egui::Context) {
+        ctx.show_viewport_immediate(
+            egui::ViewportId::from_hash_of("immediate_viewport"),
+            egui::ViewportBuilder::default()
+                .with_title("Immediate Viewport")
+                .with_inner_size([500.0, 500.0]),
+            |ctx, class| {
+                assert!(
+                    class == egui::ViewportClass::Immediate,
+                    "This backend only supports one viewport."
+                );
+                //top panel for nextcloud settings
+                egui::TopBottomPanel::top("top_panel_nextcloud").show(ctx, |ui| {
+                    egui::MenuBar::new().ui(ui, |ui| {
+                        // NOTE: no File->Quit on web pages!
+                        let is_web = cfg!(target_arch = "wasm32");
+                        if !is_web {
+                            ui.menu_button("Options", |ui| {
+                                if ui.button("Quit").clicked() {
+                                    self.show_nextcloud_viewport = false;
+                                }
+                            });
+                            ui.add_space(16.0);
+                        }
+                    });
+                });
+                egui::SidePanel::left("installation_method").show(ctx, |_ui| {
+                    });
+                egui::CentralPanel::default().show(ctx, |ui| {
+                    egui::ScrollArea::vertical().show(ui, |ui| {
+                        ui.heading("Installation Guide");
+                        ui.heading("Tip: you can click any command to copy it.");
+                        ui.add_space(20.0);
+                        self._title = String::from("Manual Installation");
+
+
+                        ui.collapsing(self._title.clone(), |ui|{
+                            ui.label("Step 1: Set up Docker");
+                            ui.label("If you have Docker installed, we can skip this step!");
+                            ui.label("To check if Docker is installed, run this command:");
+                            ui.add_space(10.0);
+                            ui.separator();
+
+
+                            if ui.label("which docker").clicked() {
+                                ui.ctx().copy_text("which docker".to_string()); //copy text to clipboard
+                                let mut toasts = Toasts::new()
+                                    .anchor(Align2::RIGHT_TOP, [10.0, 10.0])
+                                    .direction(egui::Direction::TopDown);
+
+                                toasts.add(Toast {
+                                    text: "Copied to Clipboard".into(),
+                                    kind: ToastKind::Info,
+                                    options: ToastOptions::default()
+                                        .duration_in_seconds(3.0)
+                                        .show_progress(true)
+                                        .show_icon(true),
+                                    style: Default::default(),
+                                });
+                                toasts.show(ctx);
+                            }
+                            ui.separator();
+
+
+                            ui.collapsing("Explanation of Command", |ui| {
+                                ui.label("This command finds what instance of docker is installed.");
+                                ui.label("If it returns an output, you have it installed and can go to step 2.");
+                                ui.label("If it does not return anything, continue with this step.");
+                            });
+                            ui.add_space(10.0);
+
+                            ui.label("If you do not have docker installed, we can use a simple script to install it!");
+                            ui.label("Open up your terminal and paste this command.");
+                            ui.separator();
+
+                            //convenience script to copy
+                            if ui.label("'curl -fsSL https://get.docker.com | sudo sh'").clicked() {
+                                ui.ctx().copy_text("curl -fsSL https://get.docker.com | sudo sh".to_string());
+                            }
+
+
+                            ui.separator();
+                            ui.collapsing("Explanation of command", |ui| {
+                                ui.label("This command will pull the docker convenience script and run it.");
+                                ui.label("If this failed, it means you do not have curl installed.");
+                                ui.label("Simply install 'curl' with your distribution's package manager.");
+
+
+                            });
+                            ui.add_space(20.0);
+                            ui.label("Step 2: Install Compose and Enable Services");
+                            ui.label("Next, we need to install Docker Compose. This will let us store all configuration into one file.");
+                            ui.label("Simply install it with your package manager.");
+                            ui.add_space(10.0);
+
+                            ui.label("Next, we need to enable the services.");
+                            ui.label("Use this command to enable it. Keep in mind you need to will need your sudo password.");
+                            ui.add_space(10.0);
+                            ui.separator();
+                            if ui.label("sudo systemctl enable --now docker.service \n sudo systemctl enable --now containerd.service").clicked() {
+                                ui.ctx().copy_text("sudo systemctl enable --now docker.service \n sudo systemctl enable --now containerd.service".to_string());
+                            }
+                            ui.separator();
+
+                            ui.add_space(20.0);
+                            ui.label("Step 3: Create Compose.yaml");
+                            ui.label("Create a new folder. This folder will hold your configurations in one file.");
+                            ui.label("Next, create a file and name it 'compose.yaml'.");
+                            ui.add_space(10.0);
+
+                            ui.collapsing("What does that mean?", |ui| {
+                                ui.label("A YAML file is something that can be easily read by people and computers.");
+                                ui.label("It is used to store data that can be used in other apps.");
+                                ui.label("This is what Docker will use to read your configurations");
+
+                            });
+                            ui.add_space(10.0);
+                            ui.label("Below is a configuration you can use for the application.");
+                            ui.label("Simply paste in the data and save the file.");
+                            ui.separator();
+                            ui.add_space(10.0);
+                            let mut yaml_data = "name: nextcloud-aio
+                                                services:
+                                                    nextcloud-aio-mastercontainer:
+                                                        image: ghcr.io/nextcloud-releases/all-in-one:latest
+                                                           init: true
+                                                           restart: always
+                                                           container_name: nextcloud-aio-mastercontainer
+                                                           volumes:
+                                                              - nextcloud_aio_mastercontainer:/mnt/docker-aio-config
+                                                              - /var/run/docker.sock:/var/run/docker.sock:ro
+                                                            network_mode: bridge
+                                                            ports:
+                                                              - 80:80
+                                                              - 8080:8080
+                                                              - 8443:8443
+                                                           environment:
+                                                             - SKIP_DOMAIN_VALIDATION=true
+
+                                                volumes:
+                                                   nextcloud_aio_mastercontainer:
+                                                    name: nextcloud_aio_mastercontainer";
+                            ui.collapsing("YAML data", |ui| {
+                                ui.text_edit_multiline(&mut yaml_data);
+                            });
+                            ui.add_space(20.0);
+                            ui.label("Now, we can run the service using this command:");
+                            ui.separator();
+                            if ui.label("docker compose up -d").clicked() {
+                                ui.ctx().copy_text("docker compose up".to_string());
+                            }
+                            ui.separator();
+                            ui.add_space(20.0);
+                            ui.label("Now that it is running, you can go to:");
+                            ui.separator();
+                            if ui.label("https://localhost::8443").clicked() {
+                                ui.ctx().copy_text("https://localhost:8443".to_string());
+                            }
+                            ui.separator();
+                            ui.label("And finish setting up Nextcloud!");
+
+
+
+
+
+                        });
+                        ui.collapsing("Automatic Installation", |ui| {
+                            ui.label("If you wish to use an automatic install, you can use this script to automatically configure a local installation of Nextcloud");
+                            if ui.button("Automatic Installation Script").clicked() {
+                                Command::new("sh")
+                                .arg("kitty")
+                                .spawn()
+                                .expect("failed to execute automatic install");
+                            };
+                        })
+
+
+
+                    });
+                })
+
+                });
+                if ctx.input(|i| i.viewport().close_requested()) {
+                    self.show_nextcloud_viewport = false;
+                }
+            }
+
     }
 
+
+
+
+
+
+
+impl eframe::App for PrototypeUI {
     /// Called each time the UI needs repainting, which may be many times per second.
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
+    fn update(&mut self,
+              ctx: &egui::Context,
+              _frame: &mut eframe::Frame) {
+        //top panel
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
             egui::MenuBar::new().ui(ui, |ui| {
                 // NOTE: no File->Quit on web pages!
                 let is_web = cfg!(target_arch = "wasm32");
@@ -65,52 +254,58 @@ impl eframe::App for TemplateApp {
             });
         });
         egui::SidePanel::left("quick_apps_panel").show(ctx, |ui| {
+            ui.label("Install services:");
             ui.separator();
-            ui.label("apps will go here");
+            if ui.button("Nextcloud").clicked() {
+                // let mut app_name = Application::default().name;
+                // app_name = String::from("[App] Settings");
+                //opens up new window for user to be guided through nextcloud installation
+                self.show_nextcloud_viewport = true;
+                //debug to console to check viewport
+                println!("Nextcloud is working");
+            }
+
+            if ui.button("Add new Application").clicked() {
+                ui.label("Button clicked");
+
+            }
+
         });
+        if self.show_nextcloud_viewport {
+            self.show_guide(ctx);
+        }
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("gui alpha v.0.1");
+            // central UI
+            ui.vertical_centered(|ui| {
+                ui.heading("Prototype for Demonstration");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
             });
+            ui.separator();
 
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
+            //TODO: Pull compose data from yaml file and show here
+            egui::Window::new("App Details").show(ctx, |ui| {
+                ui.add_space(16.0);
+                ui.label("App Name:");
+                ui.label("IP Address:");
+                ui.label("Port:");
+                ui.label("Status:");
+            });
 
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
                 egui::warn_if_debug_build(ui);
             });
         });
     }
+
+
+    /// Called by the framework to save state before shutdown.
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
 }
 
-fn sidebarWidget(ui: &mut egui::Ui) {
-ui.horizontal(|ui| {
 
 
-});
 
-}
-
-
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,30 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
+
+// values are hardcoded for now but I'll pull them from a db
+
+mod structs {
+    #[derive(Default)]
+    pub struct Application {
+        pub name: String,
+        pub docker_image: String,
+        pub desc: String,
+        pub ip_port: String,
+    }
+
+    impl Application {
+        pub fn name(&mut self) -> &mut String {
+            &mut self.name
+        }
+        pub fn docker_image(&mut self) -> &str {
+            &self.docker_image
+        }
+        pub fn desc(&self) -> &str {
+            &self.desc
+        }
+
+    }
+}
+
 mod app;
-pub use app::TemplateApp;
+pub use app::PrototypeUI;
